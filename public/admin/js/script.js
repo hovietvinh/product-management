8962a98c69c010b87ebbd11efeceda4413c2f2d8
hovietvinh <hovietvinh29032004@gmail.com>
2024-03-01 04:29:15 +0700

    tính năng tìm kím(front-end)

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -16,3 +16,21 @@ if(buttonStatus.length>0){
         })
     })
 }
+
+
+// FORM SEARCH 
+const formSearch = document.querySelector("#form-search");
+if(formSearch){
+    const url = new URL(window.location.href);
+    formSearch.addEventListener("submit",(e)=>{
+        e.preventDefault();
+        const keyword = e.target.elements.keyword.value;
+        if(keyword){
+            url.searchParams.set("keyword",keyword);
+        }
+        else {
+            url.searchParams.delete("keyword");
+        }
+        window.location.href = url.href
+    })
+}
