c0fc9fef4b0ae0e8a362a88c0963ea9e15aafbe8
hovietvinh <hovietvinh29032004@gmail.com>
2024-03-10 23:15:12 +0700

    Dùng thư viện multer để upload ảnh

--- routes/admin/products.route.js
+++ routes/admin/products.route.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const multer  = require('multer')
+const upload = multer({ dest: './public/uploads/' })
 const controllers = require("../../controllers/admin/products.controller");
 
 router.get('/',controllers.index );
@@ -7,7 +9,7 @@ router.patch('/change-status/:status/:id',controllers.changeStatus);
 router.patch('/change-multi',controllers.changeMulti);
 router.delete('/delete/:id',controllers.deleted);
 router.get('/create',controllers.create );
-router.post('/create',controllers.createPost );
+router.post('/create', upload.single('thumbnail'),controllers.createPost );
 
 
 
