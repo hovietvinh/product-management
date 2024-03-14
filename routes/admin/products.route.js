769ae078eec7d256917946041a0185466a24f5fd
hovietvinh <hovietvinh29032004@gmail.com>
2024-03-15 01:14:52 +0700

    đẩy file tĩnh lên cloud

--- routes/admin/products.route.js
+++ routes/admin/products.route.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const multer  = require('multer')
-const upload = multer({ dest: './public/uploads/' })
+const multer = require('multer')
+const upload = multer()
 const controllers = require("../../controllers/admin/products.controller");
 const validate = require("../../validate/products.validate")
-
-router.get('/',controllers.index );
-router.patch('/change-status/:status/:id',controllers.changeStatus);
-router.patch('/change-multi',controllers.changeMulti);
-router.delete('/delete/:id',controllers.deleted);
-router.get('/create',controllers.create );
-router.post('/create', upload.single('thumbnail'),validate.createProduct ,controllers.createPost );
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware");
+router.get('/', controllers.index);
+router.patch('/change-status/:status/:id', controllers.changeStatus);
+router.patch('/change-multi', controllers.changeMulti);
+router.delete('/delete/:id', controllers.deleted);
+router.get('/create', controllers.create);
+router.post('/create', upload.single('thumbnail'),uploadCloud.upload ,validate.createProduct, controllers.createPost);
 router.get('/edit/:id', controllers.edit);
-router.patch('/edit/:id',upload.single('thumbnail'),validate.createProduct,controllers.editPatch);
-router.get('/detail/:id',controllers.detail);
+router.patch('/edit/:id', upload.single('thumbnail'), validate.createProduct, controllers.editPatch);
+router.get('/detail/:id', controllers.detail);
 
 
 
