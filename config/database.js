a1052a0472456be1107ead1f2e695c33b78ed454
hovietvinh <hovietvinh29032004@gmail.com>
2024-03-14 23:59:31 +0700

    dung mongoDB online

--- config/database.js
+++ config/database.js
@@ -1,7 +1,9 @@
 module.exports.connect = async ()=>{
+    require("dotenv").config();
+    const MONGO_URL = process.env.MONGO_URL;
     try {
         const mongoose = require("mongoose");
-        await mongoose.connect('mongodb://127.0.0.1:27017/product-management'); 
+        await mongoose.connect(MONGO_URL); 
         console.log("connect success");
     } catch (error) {
         console.log("connect error");        
